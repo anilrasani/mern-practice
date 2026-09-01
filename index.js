7b89607d35723aad7829080afc537f6f64fe8572
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2026-09-01 22:22:46 +0530

    Extract of object

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ res.send('Home')
 
 
 
+const user  = {
+    name:'Anil',
+    age :25,
+    city : 'Tirupati'
+}
+
+// const{name,location}=user
+// console.log(name);
+// console.log(location);
+
+const {city:location } =user
+console.log(location);
+
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
