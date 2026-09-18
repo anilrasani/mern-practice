fe70c6baaf2fcf105f6eff68e4fb287569059f80
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2026-09-18 23:03:36 +0530

    greet fun with props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,13 @@
+// App.jsx
+import './App.css';
+import { Greet } from './Greet.jsx';
 
-import { useState } from 'react';
-import './App.css'
+const App = () => (
+  <div className="App">
+    <Greet name = 'Anil Rasani 1' />
+    <Greet name = 'Anil Rasani 2' />
+    <Greet name = 'Anil Rasani 3' />
+  </div>
+);
 
-
-
-const App = ()=> {
-
-    const[counter,setCounter]=useState(0);
-
-    function IncreaseCounter(){
-        setCounter(counter+1);
-    }
-    
-    return (
-        <div>
-            <h2>Employee App</h2>
-
-            <form>
-                <div>
-                    <label>Name</label>
-                    <input type="text" />
-                </div>
-
-                <div>
-                    <label>Phone</label>
-                    <input type="text" />
-                </div>
-
-                <div>
-                    <label>Address</label>
-                    <input type="text" />
-                </div>
-
-                <div>
-                    <label>Interests</label>
-                    <input type="text" />
-                </div>
-
-                <div>
-                    <label>App Used</label>
-                    <input type="text" />
-                </div>
-
-                <button type="submit">
-                    Save Employee
-                </button>
-            </form>
-            <h1>Count :{counter}</h1>
-            <button onClick={IncreaseCounter}>Increase</button>
-        </div>
-        
-    );
-}
-
-
-export default App
-
+export default App;
---
+++ frontend/src/Greet.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Greet(props){
+    return(
+        <div className="">
+            <div>Heloo {props.name}</div>
+            <h1> Hello {props.name}</h1>
+        </div>
+             
+        
+    )
+}
+
+export default Greet;
+
+// Greet.jsx
+// export const Greet = (props) => <h1>Hello {props.name}</h1>;
+
+    
+    
