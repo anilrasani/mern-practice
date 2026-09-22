341d20c1b55e4152f3a45e8969794c9edeb10544
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2026-09-22 23:03:34 +0530

    sending props from parent & looping in table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
 // App.jsx
+
 import './App.css';
-import { Greet } from './Greet.jsx';
+import './main.jsx'
+import  Greet  from './Greet.jsx';
+
 import Cbox from './tableCheckbox.jsx';
 
+const people =[
+    { id:1,name:'anil',age:25,skill:'React'},
+{ id:1,name:'anil',age:25,skill:'React'},
+{ id:1,name:'anil',age:25,skill:'React'},
+{ id:1,name:'anil',age:25,skill:'React'},
+{ id:1,name:'anil',age:25,skill:'React'}]
+
 const App = () => (
-  <div className="App">
-    <Greet name = 'Anil Rasani 1' />
-    <Greet name = 'Anil Rasani 2' />
+
+<div className="App">
+    {/* <Greet name={'anil rasani'} /> */}
+    <Greet name='anil rasani' people={people} />
+
+    {/*<Greet name = 'Anil Rasani 2' />
     <Greet name = 'Anil Rasani 3' />
-    <Cbox name = 'check box'/>
+    <Cbox name = 'check box'/> */}
+
+
   </div>
 );
 
--- frontend/src/Greet.jsx
+++ frontend/src/Greet.jsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import "./main.jsx"
 
-// function Greet(props){
-//     return(
-//         <div className="">
-//             <div>Heloo {props.name}</div>
-//             <h1> Hello {props.name}</h1>
-//         </div>
+
+const Greet= (props)=>{
+    const {name,people} = props;
+    
+    return(
+        <div className="">
+            <div>
+                <h1> Hello {props.name}</h1>
+                <h1 className="text-3xl font-bold text-purple-600">Hello Tailwind</h1>
+            
+            </div>
+            <div className="justify-center" >
+                <table>
+                <thead>
+                    <tr>
+                       <th>id</th>        
+                        <th>name</th>        
+                        <th>age</th>        
+                        <th>skill</th>        
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    {people.map((person)=>(
+                    <tr key ={person.id}>
+                    <td>{person.id}</td>
+                    <td>{person.name}</td>
+                    <td>{person.age}</td>
+                    <td>{person.skill}</td>
+                    </tr>    
+                    ))}
+                </tbody>
+            </table>
+            <div>
+                <button>
+                    Submit
+                </button>
+            </div>
+            </div>
+        </div>
              
         
-//     )
-// }
+    )
+}
 
-// export default Greet;
+export default Greet;
+
+// shortcut method or simple way to write function
 
 // Greet.jsx
-export const Greet = (props) => <h1>Hello {props.name}</h1>;
+// export const Greet = (props) => <h1>Hello {props.name}</h1>;
 
     
     
